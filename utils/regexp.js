56e80a02a5b363e7c17a0f56b029b246be4d1345
Namer, M <93187462+NamerM@users.noreply.github.com>
2022-11-04 11:34:36 +0200

    Delete utils directory

--- utils/regexp.js
+++
@@ -1,9 +0,0 @@
-const LINK_REGEXP = /^http[s]?:\/\/(www\.)?(.)?\/?(.)/i;
-const EMAIL_REGEXP = /^[a-zA-Z0-9.!#$%&‘*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-const PASS_REGEXP = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
-
-module.exports = {
-  LINK_REGEXP,
-  EMAIL_REGEXP,
-  PASS_REGEXP,
-};
