1049eadf29d0d537f7883ca746d201f1858a89fa
Melih Namer <mnamer12@outlook.com>
2022-11-04 11:13:38 +0200

    first commit with schemas

---
+++ models/article.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+const { LINK_REGEXP } = require('../utils/regex');
+
+const articleSchema = new mongoose.Schema({
+  keyword: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  source: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: string, //Date
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return LINK_REGEXP.test(v);
+      },
+      message: 'Enter a Valid URL address',
+    },
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return LINK_REGEXP.test(v);
+      },
+      message: 'Enter a Valid URL address for the Image',
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'user',
+  },
+  // likes: {
+  //   type: [mongoose.Schema.Types.ObjectId],
+  //   ref: 'user',
+  //   default: [],
+  // },
+});
+
+module.exports = mongoose.model('article', articleSchema);
