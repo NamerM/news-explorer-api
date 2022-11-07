763335fff330d8dfd6580f720863074cf882f3d3
Melih Namer <mnamer12@outlook.com>
2022-11-07 21:57:33 +0200

    fix line 15

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,6 @@ router.post('/signin', validateAuthentication, login);
 router.use(auth);
 router.use('/', userRouter);
 router.use('/', articleRouter);
-router.use(nonExisted);
+router.use('*', nonExisted);
 
 module.exports = { router };
