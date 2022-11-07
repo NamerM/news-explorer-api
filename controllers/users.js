44fbaef94a8b9d302f7a0d6d7dd4ce18bec8beb8
Melih Namer <mnamer12@outlook.com>
2022-11-07 20:05:17 +0200

    line 17

--- controllers/users.js
+++ controllers/users.js
@@ -14,7 +14,7 @@ const login = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: '7d' });
-      res.send({ data: user.toJSON(), token });
+      res.send({  token }); //data: user.toJSON(),
     })
     .catch(() => {
       next(new UnauthorizedError('Login information is incorrect, check either email or password'));
