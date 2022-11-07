4bc874f456abe9288f6ddf4ef7d3c51f61c8147f
Melih Namer <mnamer12@outlook.com>
2022-11-07 21:23:31 +0200

    fix on index.js routes

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const auth = require('../middleware/auth');
 const { createUser, login } = require('../controllers/users');
 const { userRouter } = require('./users');
 const { articleRouter } = require('./articles');
+const { nonExisted } = require('./nonExisted');
 
 router.post('/signup', validateUserBody, createUser);
 router.post('/signin', validateAuthentication, login);
